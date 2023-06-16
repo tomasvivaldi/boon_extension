f9d4f62808e3642bed521c91ca9e8046fbaa36bc
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-06-16 08:51:21 +0800

    adjusted sender subject and body fetching

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 // This is a very simplistic example. You'll need to update the selectors based on the actual email service you're targeting.
 document.addEventListener("DOMContentLoaded", function () {
-  var sender = document.querySelector(".sender").textContent;
-  var subject = document.querySelector(".subject").textContent;
-  var body = document.querySelector(".body").textContent;
+  var sender = document.querySelector(".qu span span").textContent;
+  var subject = document.querySelector("h2.hP").textContent;
+  var bodyElements = document.querySelectorAll("div.MsoNormal");
+  var body = Array.from(bodyElements)
+    .map((element) => element.textContent)
+    .join("\n");
 
   // Send the email data to the background script
   chrome.runtime.sendMessage({ emailData: { sender, subject, body } });
